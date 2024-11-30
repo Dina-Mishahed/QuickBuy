5008c3279cb89ea31d8933155dd4e175c8b231b4
ayat hashish <https://github.com/ayatHashish>
2024-11-30 14:32:08 +0200

    fire-base  test

--- projects/client/src/app/app.component.ts
+++ projects/client/src/app/app.component.ts
@@ -6,21 +6,40 @@ import { FooterComponent } from './shared/components/footer/footer.component';
 
 import * as toastr from 'toastr';
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,RouterOutlet,HeaderComponent,FooterComponent],
+  imports: [CommonModule,RouterOutlet,HeaderComponent,FooterComponent ,HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'client';
 
+  constructor(private http: HttpClient) {
 
+    this.postFunc()
+  }
   showSuccess() {
 
     toastr.success('Hello world!', 'Toastr Fun!');
+  }
+
+
+  postFunc() {
+    const data = { title: 'hi', content: 'this is rom post function rom firebace tec' }; // بيانات JSON
+
+    this.http.post('https://quickbuy-f49ad-default-rtdb.firebaseio.com/postes.json', data)
+      .subscribe(
+        response => {
+          console.log('Post successful', response);
+        },
+        error => {
+          console.error('Post failed', error);
+        }
+      );
   }
 
   showError() {
